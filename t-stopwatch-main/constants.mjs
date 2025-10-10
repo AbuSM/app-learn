4b3af88de1f762b2820d8dfb48a1670cbd539782
Fattoh <android17cpu@gmail.com>
2025-10-11 00:16:00 +0500

    remove redundant folder

--- t-stopwatch-main/constants.mjs
+++
@@ -1,4 +0,0 @@
-export const TIMER_RANGE = 50;
-export const WEATHER_API_URL =
-	"https://api.openweathermap.org/data/2.5/weather";
-export const WEATHER_API_KEY = "2304d17f87a735eba65e0703c7e2debc";
--- t-stopwatch-main/http.mjs
+++
@@ -1,3 +0,0 @@
-export async function fetcher(URL, options) {
-	return fetch(URL, options).then((res) => res.json());
-}
